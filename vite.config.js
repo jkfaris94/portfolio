ecd8e8cb066d5b5e70e37a6c33ecc336fc12fbc4
Johnny <jkfaris@gmail.com>
2025-08-19 23:29:47 -0700

    ran build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  base: "./",                // ← switch from "/portfolio/" to "./"
+  base: "./",     // important for GitHub Pages
   build: { outDir: "dist" }
 });
