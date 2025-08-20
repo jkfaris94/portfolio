39ab515facfdc4af743a19906bba163f7dc286b0
Johnny <jkfaris@gmail.com>
2025-08-19 23:52:52 -0700

    reset config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  base: "./",     // important for GitHub Pages
+  base: "/portfolio/",     // important for GitHub Pages
   build: { outDir: "dist" }
 });
